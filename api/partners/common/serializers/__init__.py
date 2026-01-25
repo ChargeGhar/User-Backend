81504b0602002048e7810c65de4438aea7279c69
itzmejanak <itzmejanak@gmail.com>
2026-01-25 11:39:02 +0545

    cleaned

--- api/partners/common/serializers/__init__.py
+++
@@ -1,9 +0,0 @@
-# api/partners/common/serializers/__init__.py
-"""
-Partner Common Serializers.
-
-Note: Auth serializers are in api.partners.auth.serializers
-"""
-
-__all__ = []
-
--- api/partners/common/services/__init__.py
+++
@@ -1,9 +0,0 @@
-# api/partners/common/services/__init__.py
-"""
-Partner Common Services.
-
-Note: Auth services are in api.partners.auth.services
-"""
-
-__all__ = []
-
